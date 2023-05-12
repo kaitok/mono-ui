df4b879941e58652b50e0bc2b9dee31434a4dedf
jo <kaitokinjo@gmail.com>
2023-05-12 22:43:47 +0900

    require to import

--- rollup.config.js
+++ rollup.config.js
@@ -2,8 +2,7 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
-
-const packageJson = require("./package.json");
+import packageJson from "./package.json" assert { type: "json" };
 
 export default [
   {
