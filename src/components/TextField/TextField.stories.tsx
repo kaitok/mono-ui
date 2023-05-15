cd7292024010b417b160650294515fb16477ca1f
jo <kaitokinjo@gmail.com>
2023-05-15 19:53:51 +0900

    CSF 3 support

--- src/components/TextField/TextField.stories.tsx
+++ src/components/TextField/TextField.stories.tsx
@@ -1,20 +1,16 @@
-import React from 'react'
-import { ComponentStory, ComponentMeta } from '@storybook/react'
+import type { Meta, StoryObj } from '@storybook/react'
 import TextField from './TextField'
 
-// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
-export default {
-  title: 'ReactComponentLibrary/TextField',
+const meta: Meta<typeof TextField> = {
+  title: 'TextField',
   component: TextField,
-} as ComponentMeta<typeof TextField>
+}
+export default meta
 
-// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template: ComponentStory<typeof TextField> = (args) => (
-  <TextField {...args} />
-)
+type Story = StoryObj<typeof TextField>
 
-export const HelloWorld = Template.bind({})
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
-HelloWorld.args = {
-  placeholder: 'Hello world!',
+export const Primary: Story = {
+  args: {
+    placeholder: 'Name',
+  },
 }
