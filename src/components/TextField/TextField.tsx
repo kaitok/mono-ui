24399d553832be53262fa472274aa43096a17909
jo <kaitokinjo@gmail.com>
2023-05-15 19:46:04 +0900

    rename

--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -4,8 +4,8 @@ export interface TextFieldProps {
   placeholder: string
 }
 
-const Button = (props: TextFieldProps) => {
+const TextField = (props: TextFieldProps) => {
   return <input type="text" placeholder={props.placeholder}></input>
 }
 
-export default Button
+export default TextField
