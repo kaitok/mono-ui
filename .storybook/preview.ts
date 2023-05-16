b7ab9137d37694fff859fcbeb38b4d55200ed23e
jo <kaitokinjo@gmail.com>
2023-05-16 15:11:08 +0900

    change default background color

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -9,6 +9,22 @@ const preview: Preview = {
         date: /Date$/,
       },
     },
+    backgrounds: {
+      default: 'gray',
+      values: [
+        {
+          name: 'gray',
+          value: '#F3F5FB',
+        },
+        {
+          name: 'white',
+          value: '#FFFFFF',
+        },
+        {
+          name: 'black',
+          value: '#000000',
+        }]
+    }
   },
 };
 
