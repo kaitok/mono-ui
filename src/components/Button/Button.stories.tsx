bc6b57fb52e52afe02f38323ee353bbf3a2893c3
jo <kaitokinjo@gmail.com>
2023-05-18 00:04:12 +0900

    remove decorators

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,11 +1,10 @@
-import type { Meta, StoryObj, Decorator } from '@storybook/react'
+import type { Meta, StoryObj } from '@storybook/react'
 
 import Button from './Button'
 
 const meta: Meta<typeof Button> = {
   title: 'Button',
   component: Button,
-  decorators: [withColorScheme],
 }
 
 export default meta
@@ -18,26 +17,4 @@ export const Primary: Story = {
     borderRadius: '0px',
     padding: '8px 20px',
   },
-}
-
-function withColorScheme(Story: StoryObj, context) {
-  let { theme } = context.globals
-  if (theme === 'light') {
-    return (
-      <>
-        <div className="color-scheme--light">
-          <Story />
-        </div>
-      </>
-    )
-  }
-  if (theme === 'dark') {
-    return (
-      <>
-        <div className="color-scheme--dark">
-          <Story />
-        </div>
-      </>
-    )
-  }
 }
