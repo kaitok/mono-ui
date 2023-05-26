8e5241eaec700f1d7628442b5a1f8f54f728c147
jo <kaitokinjo@gmail.com>
2023-05-26 23:59:08 +0900

    add radio component

---
+++ src/components/RadioGroup/RadioGroup.tsx
@@ -0,0 +1,13 @@
+export interface TextFieldProps {
+  label?: string
+}
+
+const TextField = (props: TextFieldProps) => {
+  return (
+    <>
+      <input type="radio" />
+    </>
+  )
+}
+
+export default TextField
